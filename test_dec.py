f3a79364f21e2346985bcdb7a52388be0c9b2c04
abdellahs <71831928+abdellahs@users.noreply.github.com>
2023-09-29 18:59:31 +0200

    Update test_dec.py

--- test_dec.py
+++ test_dec.py
@@ -3,27 +3,28 @@ from ode import ODEModel, solve_ode, plot_ode_solution, ODEResult
 import numpy as np
 import pytest
 
-
+### 1)a)
 model = ExponentialDecay(0.4)
-def test_ExponentialDecay():
+def test_RHS():
 
     t = 0.0
     u = np.array([3.2])
     du_dt = model(t, u)
     assert np.isclose(du_dt[0], -1.28)
 
-def test_negative_decay_raises_ValueError_a():
-    with pytest.raises(ValueError):
-        model = ExponentialDecay(-3)
 
+ # Exercise 1b)
 
-def test_negative_decay_raises_ValueError_b():
+def test_negative_decay_raises_ValueError():
     with pytest.raises(ValueError):
         model.decay = -1.0  
-              
+
+# Exercise  1d)  
+
 def test_num_states():
     assert model.num_states == 1
 
+# Exercise 1g)
 
 from exception import InvalidInitialConditionError
 def test_solve_with_different_number_of_initial_states():
@@ -34,6 +35,8 @@ def test_solve_with_different_number_of_initial_states():
     (0.4, np.array([4]), 10, 0.01),
     (0, np.array([6]), 15, .2)])
 
+
+# Exercise 1h)
 def test_solve_time(a, u0, T, dt):
     model = ExponentialDecay(a)
     res = solve_ode(model, u0, T, dt)
